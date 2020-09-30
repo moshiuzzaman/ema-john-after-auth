3680363602ee796a0950ddf2d72469896b1ebc33
moshiuzzaman <md.shozon041@gmail.com>
2020-09-30 12:52:13 +0600

    first commit

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCh3kI17PeB1m3v2HRlpBs37Fx3Pg6QjY4",
+    authDomain: "fir-practice-f0ef9.firebaseapp.com",
+    databaseURL: "https://fir-practice-f0ef9.firebaseio.com",
+    projectId: "fir-practice-f0ef9",
+    storageBucket: "fir-practice-f0ef9.appspot.com",
+    messagingSenderId: "588888313566",
+    appId: "1:588888313566:web:fefcc6899eb51a288b3c4f"
+  };
+  export default firebaseConfig;
